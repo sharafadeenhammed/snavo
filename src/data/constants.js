5e7e8e196b8030e0769929b94172e4c3f70ab5dc
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2024-03-14 16:41:13 +0100

    fixed typo in the constant.baseurl

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:5050";
-export const BASE_URL = "https://dd78-102-88-62-48.ngrok-free.app ";
+export const BASE_URL = "https://dd78-102-88-62-48.ngrok-free.app";
