3bac8beede737c4fd30eb8d6fe1c7bf5c58bf999
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2024-03-20 18:34:22 +0100

    completed theme chages on spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion"
 function Spinner() {
   return (
-    <motion.div className=' container flex-col flex items-center justify-center min-h-screen max-w-xl bg-white mx-auto fixed top-0 z-50 -translate-x-4'>
+    <motion.div className=' container flex-col flex items-center justify-center min-h-screen max-w-xl bg-slate-800 mx-auto fixed top-0 z-50 -translate-x-4'>
       <div className=' mb-4 h-20 w-20 animate-spin rounded-full border-8 border-white border-t-8 border-t-indigo-700 '>
 
       </div>
