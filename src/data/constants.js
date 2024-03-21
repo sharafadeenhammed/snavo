60c7a3056c708be71f90c5bd1e81edd0587ea9f8
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2024-03-21 12:54:11 +0100

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5050";
+//export const BASE_URL = "http://localhost:5050";
 // export const BASE_URL = "https://5dad-102-88-36-43.ngrok-free.app";
-// export const BASE_URL = "https://snavo-backend.onrender.com";
+export const BASE_URL = "https://snavo-backend.onrender.com";
