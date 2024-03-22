f7ad6a7a764bfe2299c47ef31e4d680e765f4d28
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2024-03-22 15:17:58 +0100

    updede images

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = "http://localhost:5050";
-// export const BASE_URL = "https://5dad-102-88-36-43.ngrok-free.app";
 export const BASE_URL = "https://snavo-backend.onrender.com";
